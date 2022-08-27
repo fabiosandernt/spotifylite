489df5d4af9ca86d3635d79e9a9eebbc4c071b31
Fabio Sander <fabio@easyteam.rocks>
2022-08-27 11:48:07 -0300

    migrations

--- SpotifyLite.Api/Program.cs
+++ SpotifyLite.Api/Program.cs
@@ -15,7 +15,7 @@ public class Program
 
         builder.Services
                .RegisterApplication()
-               .RegisterRepository(builder.Configuration.GetConnectionString("SpotifyLite"));
+               .RegisterRepository(builder.Configuration.GetConnectionString("Default"));
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
